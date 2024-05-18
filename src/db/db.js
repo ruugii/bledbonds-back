0347a6ee7bf54cf01acdeeb47828412e00632906
ruugii_31 <roger.barrero.sorribas@gmail.com>
2024-05-18 20:04:21 +0200

    Update database password variable name in config file

--- src/db/db.js
+++ src/db/db.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
     host: config.DB_HOST,
     user: config.DB_USER,
     database: config.DB_NAME,
-    password: config.DB_PASSWD,
+    password: config.DB_PASSWORD,
 });
 
 module.exports = pool;
