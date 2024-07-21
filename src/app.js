7c07e3439216df2a955e2d35d698f7a300225672
ruugii_31 <roger.barrero.sorribas@gmail.com>
2024-07-21 10:48:55 +0200

    implement socket.io

--- src/app.js
+++ src/app.js
@@ -1,50 +1,58 @@
+const { Server } = require('socket.io')
+const { createServer } = require('http')
 const express = require('express')
 const cors = require('cors')
-const multer = require('multer')
 const { PORT } = require('./config')
-const e = require('cors')
+const pool = require('./db/db')
 
 // Initialize express app
 const app = express()
+const server = createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+})
 
+// Socket.IO connection handling
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+
+  socket.on('chat message', async (msg) => {
+    const updateChat = async () => {
+      try {
+        pool.query('INSERT INTO chat (message, idChat) VALUES (?, ?)', [msg, msg.idChat])
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    await updateChat()
+    io.emit('chat message', msg)
+  })
+})
+
+// Set the port from config or default to 3000
 const port = PORT || 3000
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
 
+// Middleware setup
 app.use(express.json())
-
 app.use(cors())
-
 app.use(express.static('public'))
 
-app.use(
-  '/api/v1/users',
-  require('./routes/user-routes')
-)
+// Route handlers
+app.use('/api/v1/users', require('./routes/user-routes'))
+app.use('/api/v1/genders', require('./routes/genders-routes'))
+app.use('/api/v1/newsletters', require('./routes/newsletter-routes'))
+app.use('/api/v1/category', require('./routes/category-routes'))
+app.use('/api/v1/blog', require('./routes/blog-routes'))
 
-app.use(
-  '/api/v1/genders',
-  require('./routes/genders-routes')
-)
-
-app.use(
-  '/api/v1/newsletters',
-  require('./routes/newsletter-routes')
-)
-
-app.use(
-  '/api/v1/category',
-  require('./routes/category-routes')
-)
-
-app.use(
-  '/api/v1/blog',
-  require('./routes/blog-routes')
-)
-
-app.use(
-  '/api/v1/events',
-  require('./routes/events-routes')
-)
+// Uncomment and add the events routes when ready
+// app.use('/api/v1/events', require('./routes/events-routes'))
