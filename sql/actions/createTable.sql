cbaa312fead1fcfc874aa2b6c15853e290c2b3ad
ruugii_31 <roger.barrero.sorribas@gmail.com>
2024-09-05 16:39:31 +0200

    crear tabla actions

---
+++ sql/actions/createTable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `actions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `id_action` int(11) NOT NULL,
+  `id_liked` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`id_user`) REFERENCES `users` (`id`),
+  FOREIGN KEY (`id_liked`) REFERENCES `users` (`id`)
+)
