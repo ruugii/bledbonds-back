5d37c4391fdf76a568eaf5970cffbf982c60f7b8
ruugii_31 <roger.barrero.sorribas@gmail.com>
2024-12-12 13:06:33 +0100

    sql table beta users

---
+++ sql/beta/createTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `beta` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+)
